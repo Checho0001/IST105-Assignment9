5a28b164e5ceea7beaf8b5c9d5ca39cb8cf4f939
Checho0001 <sverbar@gmail.com>
2025-07-20 21:20:10 -0700

    updated url connection

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assigment9.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assignment9.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
